ae493e8bb4e47bf98d0b59b7037261b152aa8506
Elia Kuzmanova <eliamitova7@gmail.com>
2023-09-27 21:21:22 +0200

    Create RefactorSumOfOddNumbers.java

---
+++ Basic Syntax 1/src/RefactorSumOfOddNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RefactorSumOfOddNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(2 * i + 1);
+            sum += 2 * i + 1;
+        }
+        System.out.printf("Sum: %d%n", sum);
+
+    }
+}
