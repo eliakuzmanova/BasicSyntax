bc0840ab0a7faea2c85f3b367d12a35428f16136
Elia Kuzmanova <eliamitova7@gmail.com>
2023-09-27 21:21:16 +0200

    Create MultiplicationTable.java

---
+++ Basic Syntax 1/src/MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}
